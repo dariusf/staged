e6657ff346dfe84d69452722bc8a1bc3a3f6ff47
Darius Foo <darius.foo.tw@gmail.com>
2025-02-26 18:14:14 +0800

    small step for programs

--- shiftreset/Small.v
+++ shiftreset/Small.v
@@ -50,7 +50,7 @@ with expr : Type :=
   | passign (e1: val) (e2: val)
   | pif (v: val) (e1: expr) (e2: expr)
   | papp (e1: expr) (e2: expr)
-  | pshift (k: var) (e: expr)
+  | pshift (x: var) (eb: expr) (x1:var) (ek: expr)
   | preset (e: expr).
 
 #[global]
@@ -80,7 +80,7 @@ Fixpoint subst (y:var) (v:val) (e:expr) : expr :=
   | papp e v => papp e v
   | plet x t1 t2 => plet x (aux t1) (if_y_eq x t2 (aux t2))
   | pif t0 t1 t2 => pif t0 (aux t1) (aux t2)
-  | pshift k e1 => pshift k (aux e1)
+  | pshift x e1 x1 ek => pshift x (aux e1) x1 (aux ek)
   | preset e => preset (aux e)
   end.
 
@@ -158,108 +158,108 @@ Implicit Types x y z k : var.
 Implicit Types a v r : val.
 Implicit Types e : expr.
 
-Inductive satisfies : senv -> senv -> heap -> heap -> val -> flow -> flow -> Prop :=
+Inductive step_f : senv -> senv -> heap -> heap -> val -> flow -> flow -> Prop :=
 
   | s_req : forall s1 s2 H h1 h2 v f f1,
     (forall (hp hr:heap),
       H hp ->
       h1 = Fmap.union hr hp ->
       Fmap.disjoint hr hp ->
-      satisfies s1 s2 hr h2 v f f1) ->
-    satisfies s1 s2 h1 h2 v (req H f) f1
+      step_f s1 s2 hr h2 v f f1) ->
+    step_f s1 s2 h1 h2 v (req H f) f1
 
   | s_ens : forall s1 Q h1 h2 v h3,
     Q v h3 ->
     h2 = Fmap.union h1 h3 ->
     Fmap.disjoint h1 h3 ->
-    satisfies s1 s1 h1 h2 v (ens Q) fin
+    step_f s1 s1 h1 h2 v (ens Q) fin
 
   | s_seq_empty : forall v s1 h1 f2,
-    satisfies s1 s1 h1 h1 v (seq fin f2) f2
+    step_f s1 s1 h1 h1 v (seq fin f2) f2
 
   | s_seq_step : forall v s1 s2 h1 h2 f1 f2 f3,
-    satisfies s1 s2 h1 h2 v f1 f3 ->
-    satisfies s1 s2 h1 h2 v (seq f1 f2) (seq f3 f2)
+    step_f s1 s2 h1 h2 v f1 f3 ->
+    step_f s1 s2 h1 h2 v (seq f1 f2) (seq f3 f2)
 
   | s_fex : forall s1 s2 h1 h2 v (A:Type) (c:A->flow) b,
-    satisfies s1 s2 h1 h2 v (@fex A c) (c b)
+    step_f s1 s2 h1 h2 v (@fex A c) (c b)
 
   | s_fall : forall s1 s2 h1 h2 v (A:Type) (c:A->flow) f1,
-    (forall b, satisfies s1 s2 h1 h2 v (c b) f1) ->
-    satisfies s1 s2 h1 h2 v (@fall A c) f1
+    (forall b, step_f s1 s2 h1 h2 v (c b) f1) ->
+    step_f s1 s2 h1 h2 v (@fall A c) f1
 
-  | s_intersect_step_l : forall s1 s2 h1 h2 v f1 f2 f3,
-    satisfies s1 s2 h1 h2 v f1 f3 ->
-    satisfies s1 s2 h1 h2 v (intersect f1 f2) (intersect f3 f2)
+  (* | s_intersect_step_l : forall s1 s2 h1 h2 v f1 f2 f3,
+    step_f s1 s2 h1 h2 v f1 f3 ->
+    step_f s1 s2 h1 h2 v (intersect f1 f2) (intersect f3 f2)
 
   | s_intersect_step_r : forall s1 s2 h1 h2 v f1 f2 f3,
-    satisfies s1 s2 h1 h2 v f1 f3 ->
-    satisfies s1 s2 h1 h2 v (intersect f1 f2) (intersect f1 f3)
+    step_f s1 s2 h1 h2 v f1 f3 ->
+    step_f s1 s2 h1 h2 v (intersect f1 f2) (intersect f1 f3)
 
   | s_intersect_elim_l : forall s1 h1 v f2,
-    satisfies s1 s1 h1 h1 v (intersect fin f2) f2
+    step_f s1 s1 h1 h1 v (intersect fin f2) f2
 
   | s_intersect_elim_r : forall s1 h1 v f1,
-    satisfies s1 s1 h1 h1 v (intersect f1 fin) f1
+    step_f s1 s1 h1 h1 v (intersect f1 fin) f1 *)
 
   | s_disj_l : forall s1 h1 v f1 f2,
-    satisfies s1 s1 h1 h1 v (disj f1 f2) f1
+    step_f s1 s1 h1 h1 v (disj f1 f2) f1
 
   | s_disj_r : forall s1 h1 v f1 f2,
-    satisfies s1 s1 h1 h1 v (disj f1 f2) f2
+    step_f s1 s1 h1 h1 v (disj f1 f2) f2
 
   | s_unk : forall s1 h1 h1 r xf uf a,
     Fmap.read s1 xf = uf ->
-    satisfies s1 s1 h1 h1 r (unk xf a r) (uf a r)
+    step_f s1 s1 h1 h1 r (unk xf a r) (uf a r)
   .
 
 Notation "s1 ',' s2 ','  h1 ','  h2 ','  r  '|=' f1 '~>' f2" :=
-  (satisfies s1 s2 h1 h2 r f1 f2) (at level 30, only printing).
+  (step_f s1 s2 h1 h2 r f1 f2) (at level 30, only printing).
 
 
-Inductive steps :
+Inductive steps_f :
   senv -> senv -> heap -> heap -> val -> flow -> flow -> Prop :=
 
-  | steps_refl : forall s1 h1 v f,
-    steps s1 s1 h1 h1 v f f
+  | steps_f_refl : forall s1 h1 v f,
+    steps_f s1 s1 h1 h1 v f f
 
-  | steps_step : forall s1 s2 s3 h1 h2 h3 v f1 f2 f3,
-    satisfies s1 s2 h1 h2 v f1 f2 ->
-    steps s2 s3 h2 h3 v f2 f3 ->
-    steps s1 s3 h1 h3 v f1 f3.
+  | steps_f_step : forall s1 s2 s3 h1 h2 h3 v f1 f2 f3,
+    step_f s1 s2 h1 h2 v f1 f2 ->
+    steps_f s2 s3 h2 h3 v f2 f3 ->
+    steps_f s1 s3 h1 h3 v f1 f3.
 
 Notation "s1 ',' s2 ','  h1 ','  h2 ','  r  '|=' f1 '~>*' f2" :=
-  (steps s1 s2 h1 h2 r f1 f2) (at level 30, only printing).
+  (steps_f s1 s2 h1 h2 r f1 f2) (at level 30, only printing).
 
-Lemma steps_of_step : forall s1 s2 h1 h2 v f1 f2,
-  satisfies s1 s2 h1 h2 v f1 f2 ->
-  steps s1 s2 h1 h2 v f1 f2.
+Lemma steps_of_step_f : forall s1 s2 h1 h2 v f1 f2,
+  step_f s1 s2 h1 h2 v f1 f2 ->
+  steps_f s1 s2 h1 h2 v f1 f2.
 Proof using.
-  introv M. applys steps_step M. applys steps_refl.
+  introv M. applys steps_f_step M. applys steps_f_refl.
 Qed.
 
-Lemma steps_trans : forall s1 s2 s3 h1 h2 h3 v f1 f2 f3,
-  steps s1 s2 h1 h2 v f1 f2 ->
-  steps s2 s3 h2 h3 v f2 f3 ->
-  steps s1 s3 h1 h3 v f1 f3.
+Lemma steps_f_trans : forall s1 s2 s3 h1 h2 h3 v f1 f2 f3,
+  steps_f s1 s2 h1 h2 v f1 f2 ->
+  steps_f s2 s3 h2 h3 v f2 f3 ->
+  steps_f s1 s3 h1 h3 v f1 f3.
 Proof using.
   introv M1. induction M1; introv M2. { auto. } { constructors*. }
 Qed.
 
-Definition terminal f : Prop :=
+Definition terminal_f f : Prop :=
   f = fin.
 
-Definition reducible s1 h1 f1 : Prop :=
-  exists s2 h2 v f2, satisfies s1 s2 h1 h2 v f1 f2.
+Definition reducible_f s1 h1 f1 : Prop :=
+  exists s2 h2 v f2, step_f s1 s2 h1 h2 v f1 f2.
 
-Lemma reducible_inv : forall s h v,
-  ~ reducible s h fin.
+Lemma reducible_f_inv : forall s h v,
+  ~ reducible_f s h fin.
 Proof using.
   intros. intros M. inverts M. destr H. inverts H0 as H0.
 Qed.
 
-Definition notstuck s h f : Prop :=
-  terminal f \/ reducible s h f.
+Definition notstuck_f s h f : Prop :=
+  terminal_f f \/ reducible_f s h f.
 
 (* Inductive terminates : heap->trm->Prop :=
   | terminates_step : forall s t,
@@ -267,41 +267,210 @@ Definition notstuck s h f : Prop :=
       terminates s t.
 
 Definition safe (s:heap) (t:trm) : Prop :=
-  forall s' t', steps s t s' t' -> notstuck s' t'.
+  forall s' t', steps_f s t s' t' -> notstuck s' t'.
 
 Definition correct (s:heap) (t:trm) (Q:val->hprop) : Prop :=
-  forall s' v, steps s t s' v -> Q v s'. *)
+  forall s' v, steps_f s t s' v -> Q v s'. *)
 
 Example step_e1: forall (x y:loc) s1, exists s2 h2 v,
   x <> y ->
-  steps s1 s2 empty_heap h2 v (ens_ (x~~>vint 1);; ens_ (y~~>vint 2)) fin.
+  steps_f s1 s2 empty_heap h2 v (ens_ (x~~>vint 1);; ens_ (y~~>vint 2)) fin.
 Proof.
   intros. exs. intros.
 
-  applys steps_step.
+  applys steps_f_step.
   { applys s_seq_step.
     applys s_ens.
     - hintro. split. reflexivity. apply hsingle_intro.
     - fmap_eq. reflexivity.
     - fmap_disjoint. }
 
-  applys steps_step.
+  applys steps_f_step.
   { applys s_seq_empty. }
 
-  applys steps_step.
+  applys steps_f_step.
   { applys s_ens.
     - hintro. split. reflexivity. apply hsingle_intro.
     - reflexivity.
     - apply* Fmap.disjoint_single_single. }
 
+  applys steps_f_refl.
+Qed.
+
+
+(* Definition pufun := val -> val -> expr. *)
+Definition penv := Fmap.fmap var val.
+(* pufun. *)
+Implicit Types p : penv.
+
+Definition empty_penv : penv := Fmap.empty.
+
+Inductive step : penv -> heap -> expr -> heap -> expr -> Prop :=
+
+(* Context rules *)
+(* | step_seq_ctx : forall s1 s2 t1 t1' t2,
+    step s1 t1 s2 t1' ->
+    step s1 (trm_seq t1 t2) s2 (trm_seq t1' t2) *)
+| step_let_ctx : forall p h1 h2 x t1 t1' t2,
+    step p h1 t1 h2 t1' ->
+    step p h1 (plet x t1 t2) h2 (plet x t1' t2)
+
+| step_let_sh : forall p h x x1 x2 e2 ek eb,
+    step p h (plet x (pshift x1 eb x2 ek) e2)
+      h (pshift x1 eb x2 (plet x eb e2))
+
+| step_reset_val : forall p h v,
+    step p h (preset (pval v)) h (pval v)
+
+| step_reset_ctx : forall p h1 h2 e1 e2,
+    step p h1 e1 h2 e2 ->
+    step p h1 (preset e1) h2 (preset e2)
+
+| step_reset_sh : forall p h x x1 eb ek,
+    step p h (preset (pshift x eb x1 ek)) h (subst x (vfun x1 ek) eb)
+
+(* Reductions *)
+| step_fun : forall p h x t1,
+    step p h (pfun x t1) h (pval (vfun x t1))
+(* | step_fix : forall h f x t1,
+    step h (pfix f x t1) h (vfix f x t1) *)
+| step_app_fun : forall p h v1 v2 x t1,
+    v1 = vfun x t1 ->
+    step p h (papp (pval v1) (pval v2)) h (subst x v2 t1)
+
+| step_app_unk : forall p h va xf x e1,
+    Fmap.read p xf = vfun x e1 ->
+    step p h (papp (pvar xf) (pval va)) h (subst x va e1)
+
+(* | step_app_fix : forall h v1 v2 f x t1,
+    v1 = vfix f x t1 ->
+    step h (papp v1 v2) h (subst x v2 (subst f v1 t1)) *)
+| step_if : forall p h b t1 t2,
+    step p h (pif (vbool b) t1 t2) h (if b then t1 else t2)
+(* | step_seq : forall s t2 v1,
+    step s (trm_seq v1 t2) s t2 *)
+| step_let : forall p h x t2 v1,
+    step p h (plet x (pval v1) t2) h (subst x v1 t2)
+
+(* Primitive operations *)
+(* | step_add : forall s n1 n2,
+    step s (val_add (val_int n1) (val_int n2)) s (val_int (n1 + n2))
+| step_div : forall s n1 n2,
+    n2 <> 0 ->
+    step s (val_div (val_int n1) (val_int n2)) s (Z.quot n1 n2)
+| step_rand : forall s n n1,
+    0 <= n1 < n ->
+    step s (val_rand (val_int n)) s (val_int n1)
+| step_ref : forall s v p,
+    ~ Fmap.indom s p ->
+    step s (val_ref v) (Fmap.update s p v) (val_loc p)
+| step_get : forall s p,
+    Fmap.indom s p ->
+    step s (val_get (val_loc p)) s (Fmap.read s p)
+| step_set : forall s p v,
+    Fmap.indom s p ->
+    step s (val_set (val_loc p) v) (Fmap.update s p v) val_unit
+| step_free : forall s p,
+    Fmap.indom s p ->
+    step s (val_free (val_loc p)) (Fmap.remove s p) val_unit *)
+.
+
+(** The judgment [steps s t s' t'] corresponds to the reflexive and transitive
+    closure of [step]. Concretely, it asserts that the configuration [(s,t)] can
+    reduce in zero, one, or several steps to [(s',t')]. *)
+
+Inductive steps : penv -> heap -> expr -> heap -> expr -> Prop :=
+  | steps_refl : forall p h t,
+      steps p h t h t
+  | steps_step : forall p h1 h2 h3 t1 t2 t3,
+      step p h1 t1 h2 t2 ->
+      steps p h2 t2 h3 t3 ->
+      steps p h1 t1 h3 t3.
+
+Lemma steps_of_step : forall p h1 h2 t1 t2,
+  step p h1 t1 h2 t2 ->
+  steps p h1 t1 h2 t2.
+Proof using. introv M. applys steps_step M. applys steps_refl. Qed.
+
+Lemma steps_trans : forall p h1 h2 h3 t1 t2 t3,
+  steps p h1 t1 h2 t2 ->
+  steps p h2 t2 h3 t3 ->
+  steps p h1 t1 h3 t3.
+Proof using.
+  introv M1. induction M1; introv M2. { auto. } { constructors*. }
+Qed.
+
+(* Definition trm_is_val (t:trm) : Prop :=
+  match t with trm_val v => True | _ => False end.
+
+Definition reducible (s:heap) (t:trm) : Prop :=
+  exists s' t', step s t s' t'.
+
+Lemma reducible_val_inv : forall s v,
+  ~ reducible s v.
+Proof using. introv (s'&t'&M). inverts M. Qed.
+
+Definition notstuck (s:heap) (t:trm) : Prop :=
+  trm_is_val t \/ reducible s t. *)
+
+
+
+Example e1_step: exists er,
+  steps empty_penv empty_heap (pshift "k" (papp (pvar "k") (pval (vint 1))) "r" (pvar "r"))
+    empty_heap er.
+Proof.
+(* a shift on its own is terminal already *)
+Abort.
+
+Example e1_step: exists er,
+  steps (Fmap.single "k" (vfun "x" (pvar "x"))) empty_heap
+    (preset (plet "x"
+      (pshift "k" (papp (pvar "k") (pval (vint 1))) "r" (pvar "r"))
+      (padd (pvar "x") (pval (vint 1)))))
+    empty_heap er.
+Proof.
+  intros. exs. intros.
+
+  applys steps_step.
+  { applys step_reset_ctx.
+    applys step_let_sh. }
+
+  applys steps_step.
+  { applys step_reset_sh. }
+
+  simpl.
+
+  applys steps_step.
+  { applys step_app_unk.
+    resolve_fn_in_env. }
+
+  simpl.
+
   applys steps_refl.
 Qed.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 (** * Entailment *)
-Definition entails (f1 f2:flow) : Prop :=
+(* Definition entails (f1 f2:flow) : Prop :=
   forall s1 s2 h1 h2 v,
-    steps s1 s2 h1 h2 v f1 fin ->
-    steps s1 s2 h1 h2 v f2 fin.
+    steps_f s1 s2 h1 h2 v f1 fin ->
+    steps_f s1 s2 h1 h2 v f2 fin.
 
 Infix "⊑" := entails (at level 90, right associativity) : flow_scope.
 
@@ -346,11 +515,11 @@ Section Propriety.
   Qed.
 
   (* Lemma steps_seq_inv: forall s1 s2 s3 h1 h2 h3 v f1 f2,
-    steps s1 s2 h1 h2 v (f1;; f2) fin ->
-    steps s1 s3 h1 h3 v f1 fin /\ steps s3 s2 h3 h2 v f2 fin. *)
+    steps_f s1 s2 h1 h2 v (f1;; f2) fin ->
+    steps_f s1 s3 h1 h3 v f1 fin /\ steps_f s3 s2 h3 h2 v f2 fin. *)
   Lemma steps_seq_inv: forall s1 s2 h1 h2 v f1 f2,
-    steps s1 s2 h1 h2 v (f1;; f2) fin ->
-    exists s3 h3, steps s1 s3 h1 h3 v f1 fin /\ steps s3 s2 h3 h2 v f2 fin.
+    steps_f s1 s2 h1 h2 v (f1;; f2) fin ->
+    exists s3 h3, steps_f s1 s3 h1 h3 v f1 fin /\ steps_f s3 s2 h3 h2 v f2 fin.
   Proof.
     (* intros s1 s2 h1 h2 v f1. *)
     (* dependent induction f1; intros. *)
@@ -437,9 +606,9 @@ Admitted.
   Admitted. *)
 
   Lemma steps_seq_intro: forall s1 s2 s3 h1 h2 h3 v f1 f2,
-    steps s1 s3 h1 h3 v f1 fin ->
-    steps s3 s2 h3 h2 v f2 fin ->
-    steps s1 s2 h1 h2 v (f1;; f2) fin.
+    steps_f s1 s3 h1 h3 v f1 fin ->
+    steps_f s3 s2 h3 h2 v f2 fin ->
+    steps_f s1 s2 h1 h2 v (f1;; f2) fin.
   Proof.
     intros.
     dependent induction H.
@@ -447,7 +616,7 @@ Admitted.
       applys steps_step.
       eapply s_seq_empty.
       assumption. }
-    { (* f1 steps to f3, which goes to fin *)
+    { (* f1 steps_f to f3, which goes to fin *)
       applys steps_step.
       { applys s_seq_step. eassumption. }
         applys IHsteps H1.
@@ -465,4 +634,4 @@ Admitted.
     applys steps_seq_intro H H0.
   Qed.
 
-End Propriety.
+End Propriety. *)
