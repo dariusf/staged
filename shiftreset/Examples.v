3b0d84821a0a6dc40c9420d838c0701a33469c25
Darius Foo <darius.foo.tw@gmail.com>
2025-04-16 10:47:44 +0800

    Start on toss

--- shiftreset/Examples.v
+++ shiftreset/Examples.v
@@ -48,18 +48,18 @@ Module Toss.
     let r2 = k false in
     r1 + r2
 *)
-Definition s : ufun := fun _ =>
+Definition toss : ufun := fun _ =>
   sh (fun k => ∀ x a,
     bind (req (x~~>vint a) (ens_ (x~~>vint (a+1));; unk k (vbool true))) (fun r1 =>
       ∀ b, bind (req (x~~>vint b) (ens_ (x~~>vint (b+1));; unk k (vbool false))) (fun r2 =>
         ens (fun r3 => \[r3 = vadd r1 r2])))).
 
-Definition s_env := Fmap.update empty_env "s" s.
+Definition toss_env := Fmap.update empty_env "toss" toss.
 
 (* let foo () = < let v = s () in if v then 1 else 0 > *)
 Definition foo : flow :=
   rs (
-    bind (unk "s" vunit) (fun v =>
+    bind (unk "toss" vunit) (fun v =>
     ens (fun r1 => \[If v = (vbool true) then r1 = vint 1 else r1 = vint 0]))
   ).
 
@@ -67,7 +67,7 @@ Definition foo_spec : flow :=
   ∀ x a, req (x~~>vint a) (ens (fun r => x~~>vint(a+2) \* \[r=vint 1])).
 
 Theorem foo_summary : exists f,
-  entails_under s_env foo (f;; foo_spec).
+  entails_under toss_env foo (f;; foo_spec).
 Proof.
   intros.
   exists (∃ k,
@@ -75,7 +75,7 @@ Proof.
     (fun v => ens (fun r1 => \[If v = (vbool true) then r1 = vint 1 else r1 = vint 0]))))).
   unfold foo, foo_spec.
 
-  funfold1 "s". unfold s.
+  funfold1 "toss". unfold toss.
   rewrite red_init.
   rewrite red_extend.
   rewrite red_rs_sh_elim.
@@ -148,18 +148,56 @@ Proof.
   - intros. subst. f_equal.
 Qed.
 
-(* 
-Definition toss : ufun := fun n' =>
-  ∃ n r, ens_ \[vint n = n' /\ vint r = r'];;
-  req \[n > 0]
-    (disj
-      (ens_ \[n = 1];; s vunit (vint r))
-      (ens_ \[n <> 1];;
-        ∃ r1 r2, s vunit (vbool r1);;
-        unk "toss" (vint (n-1)) (vbool r2);;
-        ens (fun r => \[r = vbool (andb r1 r2)]))).
+Definition viop f a b :=
+  match a, b with
+  | vint a1, vint b1 => f a1 b1
+  | _, _ => vunit
+  end.
 
-Definition toss_env := Fmap.update empty_env "toss" toss.
+Definition vbop f a b :=
+  match a, b with
+  | vbool a1, vbool b1 => f a1 b1
+  | _, _ => vunit
+  end.
+
+Definition virel f a b :=
+  match a, b with
+  | vint a1, vint b1 => f a1 b1
+  | _, _ => False
+  end.
+
+Notation vgt a b := (virel (fun x y => x > y) a b).
+Notation veq a b := (virel (fun x y => x = y) a b).
+Notation vneq a b := (virel (fun x y => x <> y) a b).
+Notation vsub a b := (viop (fun x y => x - y) a b).
+Notation vand a b := (vbop (fun x y => x && y) a b).
+
+Coercion vint : Z >-> val.
+Coercion vbool : bool >-> val.
+
+(*
+
+let toss_n n =
+  if n = 1 then
+    toss ()
+  else
+    let r1 = toss () in
+    let r2 = toss_n (n - 1) in
+    r1 && r2
+
+*)
+Definition toss_n : ufun := fun (n:val) =>
+  req \[vgt n (vint 0)]
+    (disj
+      (ens_ \[veq n 1];; toss vunit)
+      (ens_ \[vneq n 1];;
+        bind (toss vunit) (fun r1 =>
+        bind (unk "toss" (vsub n 1)) (fun r2 =>
+        ens (fun r => \[r = vand r1 r2]))))).
+
+Definition toss_n_env := Fmap.update empty_env "toss" toss.
+
+(* 
 
 Definition foon n r : flow :=
   rs (
