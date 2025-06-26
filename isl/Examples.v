cc2a859e8af98b90a0c446dfe4833c9bc252dd05
Darius Foo <darius.foo.tw@gmail.com>
2025-06-26 10:30:23 +0800

    Add errors and heap

---
+++ isl/Examples.v
@@ -0,0 +1,11 @@
+
+(* From ISL Require Import Logic Automation Entl. *)
+(* From ISL Require ExamplesEnt. *)
+From ISL Require Import Basics Norm.
+Local Open Scope string_scope.
+
+
+Lemma entl_elim_bind: forall P fk f1 (f:var) u H,
+  (forall r, P r -> entails (defun f u;; ens_ H;; rs (fk r)) f1) ->
+    entails (defun f u;; ens_ H;; rs (bind (ens (fun r => \[P r])) fk)) f1.
+Proof.
