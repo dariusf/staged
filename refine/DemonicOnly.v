f97ebd95fc737e576becea8a0b7df87af29b7745
Darius Foo <darius.foo.tw@gmail.com>
2025-07-31 15:15:25 +0800

    Pre/post model

--- refine/DemonicOnly.v
+++ refine/DemonicOnly.v
@@ -5,11 +5,11 @@ From Coq Require Import Lia.
 Set Implicit Arguments.
 
 (* demonic nondet only *)
-Definition wp (A : Type) : Type := (A -> Prop) -> Prop.
+Definition W (A : Type) : Type := (A -> Prop) -> Prop.
 
 
 Lemma sdemon_weaken :
- forall (A : Type) (s : wp A) (Q1 Q2 : A -> Prop),
+ forall (A : Type) (s : W A) (Q1 Q2 : A -> Prop),
  s Q1 -> (forall a : A, Q1 a -> Q2 a) -> s Q2.
 Proof.
   intros.
@@ -17,8 +17,9 @@ Proof.
   (* intros A s Q1 Q2 H; case (DScorrect s Q1); case (DScorrect s Q2);
    intuition eauto.
 Qed. *)
+Abort.
 
-Definition entails (A : Type) (s1 s2 : wp A) :=
+Definition entails (A : Type) (s1 s2 : W A) :=
   forall Q : A -> Prop, (s2 Q -> s1 Q).
 
 (* preorder *)
@@ -32,13 +33,13 @@ Proof.
 Qed.
 
 
-Definition val (A : Type) (a:A) : wp A
+Definition val (A : Type) (a:A) : W A
  := fun Q => Q a.
 
-Definition bind (A B : Type) (s1:wp A) (s2:(A -> wp B)) : wp B
+Definition bind (A B : Type) (s1:W A) (s2:(A -> W B)) : W B
  := fun Q => s1 (fun a => (s2 a) Q).
 
-Definition seq (A B : Type) (s1:wp A) (s2:wp B) : wp B
+Definition seq (A B : Type) (s1:W A) (s2:W B) : W B
  := bind s1 (fun (_:A) => s2).
 
 (* Infix ";;" := seq (at level 38, right associativity). *)
@@ -49,20 +50,20 @@ Notation "c1 ;; c2" := (bind c1 (fun _ => c2)) (at level 100, right associativit
 Notation "x <- c1 ;; c2" := (bind c1 (fun x => c2))
                              (right associativity, at level 84, c1 at next level).
 
-Definition require (p:Prop) : wp unit := fun Q => p /\ Q tt.
+Definition require (p:Prop) : W unit := fun Q => p /\ Q tt.
 
-Definition ensure (p:Prop) : wp unit := fun Q => p -> Q tt.
+Definition ensure (p:Prop) : W unit := fun Q => p -> Q tt.
 
-Definition any (A : Type) : wp A := fun Q => forall a : A, Q a.
+Definition any (A : Type) : W A := fun Q => forall a : A, Q a.
 
-Definition abort (A : Type) : wp A := fun Q => False.
+Definition abort (A : Type) : W A := fun Q => False.
 
-Definition magic (A : Type) : wp A := fun Q => True.
+Definition magic (A : Type) : W A := fun Q => True.
 
-Definition or (A:Type) (s1 s2:(wp A)) : wp A := fun Q => s1 Q /\ s2 Q.
+Definition or (A:Type) (s1 s2:(W A)) : W A := fun Q => s1 Q /\ s2 Q.
 
 
-Lemma ex1: forall x,
+Example ex1: forall x,
   entails (require (x >= 1)) (require (x > 1)).
 Proof.
   intros.
@@ -72,7 +73,7 @@ Proof.
   assumption.
 Qed.
 
-Lemma ex2: forall x,
+Example ex2: forall x,
   entails (require (x = 1))
     (bind (val 1) (fun y => require (x + 1 = 2))).
 Proof.
@@ -83,9 +84,9 @@ Proof.
   assumption.
 Qed.
 
-Definition f x := (ensure (x = x + 1)).
+Definition f x := (ensure (x = 1)).
 
-Lemma ex3: forall x,
+Example ex3: forall x,
   x >= 0 ->
   entails (f x)
     (ensure (x >= 1)).
@@ -93,10 +94,11 @@ Proof.
   intros.
   unfold entails, ensure, f, ensure.
   intros.
+  apply H0.
   lia.
 Qed.
 
-Lemma wp_extens: forall (A:Type) (f:wp A) (p1 p2:A->Prop),
+Lemma wp_extens: forall (A:Type) (f:W A) (p1 p2:A->Prop),
   (forall v, p1 v -> p2 v) ->
   f p1 -> f p2.
 Proof.
@@ -106,7 +108,7 @@ Proof.
 (* Qed. *)
 Admitted.
 
-Lemma seq_cancel: forall (f:wp unit) (p1 p2:wp unit),
+Lemma seq_cancel: forall (f:W unit) (p1 p2:W unit),
   entails p1 p2 ->
   entails (f;; p1) (f;; p2).
 Proof.
@@ -116,7 +118,7 @@ Proof.
   auto.
 Qed.
 
-Lemma bind_cancel: forall (A:Type) (f:wp A) (p1 p2:A -> wp unit),
+Lemma bind_cancel: forall (A:Type) (f:W A) (p1 p2:A -> W unit),
   (forall x, entails (p1 x) (p2 x)) ->
   entails (x <- f;; p1 x) (x <- f;; p2 x).
 Proof.
@@ -127,7 +129,7 @@ Proof.
 (* Qed. *)
 Abort.
 
-Lemma seq_cancel1: forall (f:wp unit) (p1 p2:wp unit),
+Lemma seq_cancel1: forall (f:W unit) (p1 p2:W unit),
   entails (f;; p1) (f;; p2) ->
   entails p1 p2.
 Proof.
@@ -139,7 +141,7 @@ Proof.
 (* Qed. *)
 Abort.
 
-Lemma ex4: forall (f:nat -> wp unit) x,
+Example ex4: forall (f:nat -> W unit) x,
   entails (f x;; ensure (x = 0))
     (f x;; ensure (x > 0)).
 Proof.
---
+++ refine/PrePost.v
@@ -0,0 +1,90 @@
+
+From Coq Require Import Classes.RelationClasses.
+From Coq Require Import Lia.
+
+Set Implicit Arguments.
+
+Record W (A:Type): Type := pre_post { pre: Prop; post: A -> Prop }.
+
+Definition refines (A:Type) (s1 s2:W A): Prop
+:= pre s2 -> ( pre s1 /\ (forall a:A, post s1 a -> post s2 a) ).
+
+Definition val (A:Type) (a:A): W A := pre_post True (fun b => a=b).
+
+Definition bind (A B:Type) (s1:W A) (s2:A -> W B): (W B) :=
+  pre_post (pre s1 /\ (forall a:A, post s1 a -> pre (s2 a)))
+    (fun b => exists a:A, post s1 a /\ post (s2 a) b).
+
+Definition seq (A B : Type) (s1:W A) (s2:W B) : W B
+ := bind s1 (fun (_:A) => s2).
+
+(* Infix ";;" := seq (at level 38, right associativity). *)
+
+Notation "c1 ;; c2" := (bind c1 (fun _ => c2)) (at level 100, right associativity).
+
+(* https://www.cs.cornell.edu/courses/cs6115/2017fa/notes/Monads.html *)
+Notation "x <- c1 ;; c2" := (bind c1 (fun x => c2))
+                             (right associativity, at level 84, c1 at next level).
+
+Definition any (A:Type): W A := pre_post True (fun a:A => True).
+
+Definition sync (A B:Type) (s:A -> W B) : W B :=
+  pre_post (exists a:A, pre (s a))
+    (fun b => forall a:A, pre (s a) -> post (s a) b).
+
+Definition require (p:Prop) : W unit := pre_post p (fun _ => True).
+
+Definition ensure (p:Prop) : W unit := pre_post True (fun _ => p).
+
+Notation entails := refines.
+
+Example ex1: forall x,
+  entails (require (x >= 1)) (require (x > 1)).
+Proof.
+  intros.
+  unfold refines, require. intros.
+  simpl in *.
+  split. lia.
+  auto.
+Qed.
+
+Example ex2: forall x,
+  entails (require (x = 1))
+    (bind (val 1) (fun y => require (x + 1 = 2))).
+Proof.
+  intros.
+  unfold entails, require. simpl. intros (?&?).
+  specialize (H0 1 ltac:(reflexivity)).
+  split.
+  lia.
+  intros.
+  exists 1.
+  split.
+  lia.
+  constructor.
+Qed.
+
+Definition f x := (ensure (x = 1)).
+
+Example ex3: forall x,
+  x >= 0 ->
+  entails (f x)
+    (ensure (x >= 1)).
+Proof.
+  intros.
+  unfold entails, ensure, f, ensure. simpl. intros.
+  split. constructor.
+  lia.
+Qed.
+
+Example ex4: forall (f:nat -> W unit) x,
+  entails (f x;; ensure (x = 0))
+    (f x;; ensure (x >= 0)).
+Proof.
+  unfold entails, ensure. simpl. intros.
+  split. assumption.
+  intros.
+  destruct H0 as (?&?&?).
+  exists x0.
+  split. assumption. lia.
+Qed.
