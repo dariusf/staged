7b8efb6cc1b0ab07846dca7f9db4f94547773f55
Darius Foo <darius.foo.tw@gmail.com>
2025-11-10 17:40:18 +0800

    Variable arity, but some errors are swallowed

--- ctx-equiv-ixfree/ixfree_tactics.v
+++ ctx-equiv-ixfree/ixfree_tactics.v
@@ -10,22 +10,45 @@ Proof.
   assumption.
 Abort.
 
-Ltac ispec H H1 :=
+Ltac ispec_one H H1 k :=
   match type of H with
+  | I_valid_at _ (I_forall _ _) =>
+    ispecialize H H1; k
   | I_valid_at _ (I_prop (forall _, _)) =>
-    ispecialize H H1
+    ispecialize H H1; k
   | I_valid_at _ (I_prop (forall _, forall _, _)) =>
-    ispecialize H H1
+    ispecialize H H1; k
   | I_valid_at _ (I_arrow (I_prop _) _) =>
     refine ((fun H2 => _) (I_arrow_elim _ _ H (I_prop_intro _ H1)));
     clear H;
-    rename H2 into H
+    rename H2 into H; k
   | I_valid_at _ (I_arrow _ _) =>
     refine ((fun H2 => _) (I_arrow_elim _ _ H H1));
     clear H;
-    rename H2 into H
+    rename H2 into H; k
   (* | _ => idtac "no match" *)
   end.
+
+Tactic Notation "ispec" hyp(H) uconstr(t1) :=
+  ispec_one H t1 idtac.
+
+Tactic Notation "ispec" hyp(H) uconstr(t1) uconstr(t2) :=
+  ispec_one H t1 ltac:(ispec H t2).
+
+Tactic Notation "ispec" hyp(H) uconstr(t1) uconstr(t2) uconstr(t3) :=
+  ispec_one H t1 ltac:(ispec H t2 t3).
+
+Tactic Notation "ispec" hyp(H) uconstr(t1) uconstr(t2) uconstr(t3) uconstr(t4) :=
+  ispec_one H t1 ltac:(ispec H t2 t3 t4).
+
+Tactic Notation "ispec" hyp(H) uconstr(t1) uconstr(t2) uconstr(t3) uconstr(t4) uconstr(t5) :=
+  ispec_one H t1 ltac:(ispec H t2 t3 t4 t5).
+
+Tactic Notation "ispec" hyp(H) uconstr(t1) uconstr(t2) uconstr(t3) uconstr(t4) uconstr(t5)uconstr(t6) :=
+  ispec_one H t1 ltac:(ispec H t2 t3 t4 t5 t6).
+
+Tactic Notation "ispec" hyp(H) uconstr(t1) uconstr(t2) uconstr(t3) uconstr(t4) uconstr(t5)uconstr(t6) uconstr(t7):=
+  ispec_one H t1 ltac:(ispec H t2 t3 t4 t5 t6 t7).
 
 Example test_ispec2 P R Q n :
   P → R → (n ⊨ (P → R → Q)ᵢ) → Q.
@@ -58,5 +81,23 @@ Proof.
   intros.
   Fail ispecialize H0 H.
   ispec H0 H.
+  assumption.
+Abort.
+
+Example test_ispec3 P R Q n :
+  P → R → (n ⊨ (P)ᵢ →ᵢ (R)ᵢ →ᵢ Q) → n ⊨ Q.
+Proof.
+  intros Hp Hr **.
+  Fail ispecialize H0 H.
+  ispec H Hp Hr.
+  assumption.
+Abort.
+
+Example test_ispec4 A (y:A) (P:A→Prop) Q n :
+  P y → (n ⊨ ∀ᵢ x, (P x)ᵢ →ᵢ Q) → n ⊨ Q.
+Proof.
+  intros Hp H **.
+  Fail ispecialize H0 H.
+  ispec H y Hp.
   assumption.
 Abort.
