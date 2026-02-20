6f4ee08277b5984084c59daba8961312a6ad63cc
Darius Foo <darius.foo.tw@gmail.com>
2026-02-20 15:47:39 +0800

    Install IxFree

--- package-artifact.sh
+++ package-artifact.sh
@@ -11,7 +11,14 @@ cp -r IxFree ctx-equiv-ixfree Binding shiftreset staged slf $BUILD
 # edit files
 cat _CoqProject | grep -v iris | grep -v future | grep -v types > $BUILD/_CoqProject
 printf 'Makefile.coq:\n\trocq makefile -f _CoqProject -o Makefile.coq\n-include Makefile.coq' > $BUILD/Makefile
-echo 'Rocq 9.0.1 is required. make will compile the development and its dependencies, which are all included. It should take 2-3 minutes.
+echo 'Rocq 9.0.1 is required. To compile the development and its dependencies,
+
+```sh
+(cd IxFree; make && make install)
+make
+```
+
+This should take 2-3 minutes.
 
 The simple formalisation is in the shiftreset directory, while the one based on contextual equivalence is in ctx-equiv-ixfree.' > $BUILD/readme.md
 
