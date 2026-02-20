e8f68dad43c2754f576528e4ae0b6874291b516c
Darius Foo <darius.foo.tw@gmail.com>
2026-02-20 12:25:31 +0800

    Add artifact packaging script

---
+++ package-artifact.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+set -x
+PREBUILD=$(mktemp -d)
+mkdir $PREBUILD/rocq
+BUILD=$PREBUILD/rocq
+
+# git clone https://github.com/dariusf/IxFree.git
+cp -r IxFree ctx-equiv-ixfree Binding shiftreset staged slf $BUILD
+
+# edit files
+cat _CoqProject | grep -v iris | grep -v future | grep -v types > $BUILD/_CoqProject
+printf 'Makefile.coq:\n\trocq makefile -f _CoqProject -o Makefile.coq\n-include Makefile.coq' > $BUILD/Makefile
+echo 'Rocq 9.0.1 is required. make will compile the development and its dependencies, which are all included. It should take 2-3 minutes.
+
+The simple formalisation is in the shiftreset directory, while the one based on contextual equivalence is in ctx-equiv-ixfree.' > $BUILD/readme.md
+
+cd $PREBUILD
+zip -r rocq.zip rocq
+
+cd -
+cp $PREBUILD/rocq.zip .
