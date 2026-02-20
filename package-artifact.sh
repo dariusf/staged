0209e6ae1410b70e23a970a2a75bea15c7d4fab2
Darius Foo <darius.foo.tw@gmail.com>
2026-02-20 16:05:49 +0800

    Redact, add stdpp

--- package-artifact.sh
+++ package-artifact.sh
@@ -9,9 +9,14 @@ BUILD=$PREBUILD/rocq
 cp -r IxFree ctx-equiv-ixfree Binding shiftreset staged slf $BUILD
 
 # edit files
+rm -rf $BUILD/IxFree/.git
 cat _CoqProject | grep -v iris | grep -v future | grep -v types > $BUILD/_CoqProject
 printf 'Makefile.coq:\n\trocq makefile -f _CoqProject -o Makefile.coq\n-include Makefile.coq' > $BUILD/Makefile
-echo 'Rocq 9.0.1 is required. To compile the development and its dependencies,
+echo 'The development compiles with Rocq 9.0.1.
+
+The stdpp library is required. You may install it following the instructions here: https://gitlab.mpi-sws.org/iris/stdpp.
+
+To compile the development and its dependencies,
 
 ```sh
 (cd IxFree; make && make install)
